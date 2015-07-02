a6514b69e76b3d4063f3954ca9fd364271716159
hefangshi <hefangshi@gmail.com>
2015-07-02 14:11:16 +0800

    [add] support app scaffold

--- config/scaffold.js
+++ config/scaffold.js
@@ -11,12 +11,10 @@ module.exports = {
                 required: true,
                 'default': 'home'
             }],
-            'roadmap': [
-                {
-                    reg: '**',
-                    release: '/${app_name}/$&'
-                }
-            ]
+            'roadmap': [{
+                reg: '**',
+                release: '/${app_name}/$&'
+            }]
         }
     },
     'project': {
@@ -31,12 +29,138 @@ module.exports = {
                 required: true,
                 'default': 'yog'
             }],
-            'roadmap': [
-                {
-                    reg: '**',
-                    release: '/${project_name}/$&'
+            'roadmap': [{
+                reg: '**',
+                release: '/${project_name}/$&'
+            }]
+        }
+    },
+    'widget': {
+        'info': 'create a new widget',
+        'config': {
+            'type': 'github',
+            'repos': 'fex-team/yog2-app-scaffold-template',
+            'needApp': true,
+            'path': '/widget',
+            'allowCustom': true,
+            'customPath': 'yog2_template',
+            'prompt': [{
+                name: 'widget_name',
+                description: 'Enter your widget name (eg. header or book/list)',
+                type: 'string',
+                required: true
+            }],
+            'property': [{
+                name: 'file_name',
+                from: 'widget_name',
+                calc: function (widget_name) {
+                    return widget_name.split('/').pop();
                 }
-            ]
+            }, {
+                name: 'upper-file_name',
+                from: 'file_name',
+                calc: function (file_name) {
+                    return file_name.replace(/^(\w)/, function (v) {
+                        return v.toUpperCase();
+                    });
+                }
+            }],
+            'roadmap': [{
+                reg: /(.*)(\$\{widget_name\})(.*)/,
+                release: '/client/widget/${widget_name}/${file_name}$3'
+            }]
+        }
+    },
+    'page': {
+        'info': 'create a new page',
+        'config': {
+            'type': 'github',
+            'repos': 'fex-team/yog2-app-scaffold-template',
+            'needApp': true,
+            'path': '/page',
+            'allowCustom': true,
+            'customPath': 'yog2_template',
+            'prompt': [{
+                name: 'page_name',
+                description: 'Enter your page url path',
+                type: 'string',
+                required: true,
+                'default': '/book/detail'
+            }, {
+                name: 'ral_name',
+                description: 'Enter main ral service name',
+                type: 'string',
+                required: true,
+                'default': 'BACKEND'
+            }],
+            'property': [{
+                name: 'file_name',
+                from: 'page_name',
+                calc: function (page_name) {
+                    return page_name.split('/').pop();
+                }
+            }, {
+                name: 'upper-file_name',
+                from: 'file_name',
+                calc: function (file_name) {
+                    return file_name.replace(/^(\w)/, function (v) {
+                        return v.toUpperCase();
+                    });
+                }
+            }],
+            'roadmap': [{
+                reg: /(.*)(\$\{page_name\})(.*)/,
+                release: '/$1/${page_name}$3'
+            }, {
+                reg: '**',
+                release: '/$&'
+            }]
+        }
+    },
+    'api': {
+        'info': 'create a api',
+        'config': {
+            'type': 'github',
+            'repos': 'fex-team/yog2-app-scaffold-template',
+            'path': '/api',
+            'needApp': true,
+            'allowCustom': true,
+            'customPath': 'yog2_template',
+            'prompt': [{
+                name: 'api_name',
+                description: 'Enter your api url path',
+                type: 'string',
+                required: true,
+                'default': '/api/getBookInfo'
+            }, {
+                name: 'ral_name',
+                description: 'Enter main ral service name',
+                type: 'string',
+                required: true,
+                'default': 'BACKEND'
+            }],
+            'property': [{
+                name: 'file_name',
+                from: 'api_name',
+                calc: function (api_name) {
+                    return api_name.split('/').pop();
+                }
+            }, {
+                name: 'upper-file_name',
+                from: 'file_name',
+                calc: function (file_name) {
+                    return file_name.replace(/^(\w)/, function (v) {
+                        return v.toUpperCase();
+                    });
+                }
+            }],
+            'roadmap': [{
+                reg: /(.*)(\$\{api_name\})(.*)/,
+                release: '/$1/${api_name}$3'
+            }, {
+                reg: '**',
+                release: '/$&'
+            }]
         }
     },
     'node-runtime': {
@@ -45,12 +169,10 @@ module.exports = {
             'type': 'gitlab',
             'repos': 'fex/node-runtime',
             'prompt': [],
-            'roadmap': [
-                {
-                    reg: '**',
-                    release: '/$&'
-                }
-            ]
+            'roadmap': [{
+                reg: '**',
+                release: '/$&'
+            }]
         }
-    } 
+    }
 };
--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 'use strict';
 
+/* eslint-disable fecs-camelcase, camelcase, max-nested-callbacks */
+
 var spawn = require('child_process').spawn;
 
 exports.name = 'init';
@@ -12,15 +14,14 @@ exports.desc = 'A awesome scaffold of fis';
 
 var templates = require('./config/scaffold.js');
 var fs = require('fs');
+var path = require('path');
 
 
-exports.register = function(commander) {
-    var o_args = process.argv;
- 
+exports.register = function (commander) {
     commander
         .option('--verbose', 'output verbose help', Boolean, false);
 
-    fis.util.map(templates, function(key, info){
+    fis.util.map(templates, function (key, info) {
         commander
             .command(key)
             .description('create a ' + key);
@@ -43,46 +44,83 @@ exports.register = function(commander) {
 
         template = template.split('@');
 
-        var version = template.length == 2 ? template[1] : 'master';
+        var version = template.length === 2 ? template[1] : 'master';
 
         var name = template[0];
-        if (name === 'yog'){
+        if (name === 'yog') {
             name = 'project';
         }
 
         var conf = templates[name];
 
-        if (!conf){
+        if (!conf) {
             fis.log.error('invalid init command, see -h');
         }
-        var scaffold = new (require('fis-scaffold-kernel'))({
+
+        var dir = process.cwd();
+
+        // 检测是否是APP目录
+        if (conf.config.needApp) {
+            try {
+
+                fs.statSync(path.join(dir, 'fis-conf.js'));
+                fs.statSync(path.join(dir, 'server'));
+                fs.statSync(path.join(dir, 'client'));
+                require(path.join(dir, 'fis-conf.js'));
+            }
+            catch (e) {
+                fis.log.error('current folder is not a valid app folder');
+            }
+        }
+
+
+        var scaffold = new(require('fis-scaffold-kernel'))({
             type: conf.config.type,
             log: {
-                level: 4 //default show all log; set `0` == silent.
+                level: 4 // default show all log; set `0` == silent.
             }
         });
         fis.log.notice('Downloading and unzipping...');
-        var dir = process.cwd();
-        var prompts = null;
         var keyword_reg = conf.config.keyword_reg || /\{\{-([\s\S]*?)-\}\}/ig;
-        scaffold.download(conf.config.repos + '@' + version, function (err, tmp_path) {
-            if (err){
-                fis.log.error(err);
-            }
-            var files = scaffold.util.find(tmp_path);
+
+        if (conf.config.allowCustom && conf.config.customPath) {
+            var customPath = path.join(dir, conf.config.customPath);
+            fis.log.notice('Using custom template from ' + customPath);
+            var tmp_path = fis.project.getTempPath(path.join('custom_template', +new Date() + ''));
+            scaffold.util.copy(customPath, tmp_path);
+            deploy(tmp_path);
+        }
+        else {
+            scaffold.download(conf.config.repos + '@' + version, function (err, tmp_path) {
+                if (err) {
+                    fis.log.error(err);
+                }
+                deploy(tmp_path);
+            });
+        }
+
+        function deploy(templatePath) {
+            var source_path = path.join(templatePath, conf.config.path);
+            var files = scaffold.util.find(source_path);
             scaffold.prompt(conf.config.prompt, function (err, results) {
                 if (err) {
                     fis.log.error(err);
                 }
-                prompts = results;
+                if (conf.config.property) {
+                    conf.config.property.forEach(function (property) {
+                        results[property.name] = property.calc(results[property.from]);
+                    });
+                }
+                results._namespace = fis.config.get('namespace');
                 fis.util.map(results, function (k, v) {
                     fis.util.map(files, function (index, filepath) {
-                        if (fs.lstatSync(filepath).isSymbolicLink() === false && fis.util.isTextFile(filepath)){
+                        if (fs.lstatSync(filepath).isSymbolicLink() === false && fis.util.isTextFile(
+                            filepath)) {
                             var content = fis.util.fs.readFileSync(filepath, {
                                 encoding: 'utf8'
                             });
                             content = content.replace(keyword_reg, function (m, $1) {
-                                if ($1 == k) {
+                                if ($1 === k) {
                                     m = v;
                                 }
                                 return m;
@@ -91,14 +129,14 @@ exports.register = function(commander) {
                         }
                     });
                 });
-                conf.config.roadmap.forEach(function(ruler){
+                conf.config.roadmap.forEach(function (ruler) {
                     fis.util.map(results, function (k, v) {
-                        ruler.release = ruler.release.replace('${' + k + '}', v);
+                        ruler.release = ruler.release.replace(new RegExp('\\$\\{' + k + '\\}', 'g'), v);
                     });
                 });
-                scaffold.deliver(tmp_path, dir, conf.config.roadmap);
+                scaffold.deliver(source_path, dir, conf.config.roadmap);
                 fis.log.notice('Done');
             });
-        });
+        }
     });
 };
